ed070bcc947cfca573f90e1f5ff4cff88f28500f
Wellington Moreno <jwellington.moreno@gmail.com>
2017-07-07 16:28:03 -0700

    Creates Pug Cell

---
+++ DisneyCodingChallenge/DisneyCodingChallenge/PugCell.swift
@@ -0,0 +1,15 @@
+//
+//  PugCell.swift
+//  DisneyCodingChallenge
+//
+//  Created by Wellington Moreno on 7/7/17.
+//  Copyright © 2017 Disney, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PugCell: UICollectionViewCell {
+    
+    
+}
